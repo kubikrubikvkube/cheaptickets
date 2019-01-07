f1b8717db61eec1450a37030010e59d0a78d8574
snesh <v.raskulin@gmail.com>
2019-01-08 00:06:43 +0300

    31

--- src/main/java/com/example/tickets/AppConfig.java
+++ src/main/java/com/example/tickets/AppConfig.java
@@ -6,9 +6,13 @@ import com.example.tickets.ticket.TicketDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@ComponentScan
+@EnableWebMvc
 public class AppConfig {
 
     @Bean
