5d64361eaf315de4e611a07751fb66d0cb842b14
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-01-11 17:48:13 +0300

    34

---
+++ src/main/java/com/example/tickets/statistics/TicketStatistics.java
@@ -0,0 +1,23 @@
+package com.example.tickets.statistics;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "ticket_statistics_routes", indexes = {@Index(name = "idx_ticket_statistics_routes", columnList = "origin,destination")})
+public class TicketStatistics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String origin;
+
+    private String destination;
+
+    @CollectionTable(name = "ticket_statistics_by_day_collection")
+    @OneToMany(targetEntity = TicketStatisticsByDay.class, cascade = CascadeType.ALL)
+    private List<TicketStatisticsByDay> ticketStatisticsByDay;
+}
