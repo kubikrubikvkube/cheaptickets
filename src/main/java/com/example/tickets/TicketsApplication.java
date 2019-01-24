b92ca269592d862b96d9f9d58af6c32f89dcf738
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-01-24 18:01:07 +0300

    50

--- src/main/java/com/example/tickets/TicketsApplication.java
+++ src/main/java/com/example/tickets/TicketsApplication.java
@@ -3,11 +3,18 @@ package com.example.tickets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TicketsApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicketsApplication.class, args);
     }
 
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
 
