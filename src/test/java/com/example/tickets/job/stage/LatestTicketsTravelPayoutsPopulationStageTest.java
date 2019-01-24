2e343817c1a50a6abfdbe6c550c873bc8b612c0c
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-01-24 11:47:32 +0300

    48

--- src/test/java/com/example/tickets/job/stage/LatestTicketsTravelPayoutsPopulationStageTest.java
+++ src/test/java/com/example/tickets/job/stage/LatestTicketsTravelPayoutsPopulationStageTest.java
@@ -7,26 +7,36 @@ import com.example.tickets.travelpayouts.TravelPayoutsService;
 import com.example.tickets.travelpayouts.request.LatestRequest;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
-import org.apache.commons.lang3.NotImplementedException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 class LatestTicketsTravelPayoutsPopulationStageTest {
+    private final Logger log = LoggerFactory.getLogger(LatestTicketsTravelPayoutsPopulationStageTest.class);
+    @SpyBean
+    private TravelPayoutsService travelPayoutsService;
+
+    @SpyBean
+    private SubscriptionService subscriptionService;
+
+    @SpyBean
+    private TicketService ticketService;
 
     @Test
     void if_ticket_not_exist_ticket_should_be_saved() {
-        TravelPayoutsService travelPayoutsService = mock(TravelPayoutsService.class);
-        SubscriptionService subscriptionService = mock(SubscriptionService.class);
-        TicketService ticketService = mock(TicketService.class);
-
         LatestRequest ledMowLatestRequest = LatestRequest.builder()
                 .origin("A")
                 .destination("B")
@@ -45,16 +55,13 @@ class LatestTicketsTravelPayoutsPopulationStageTest {
         when(ticketService.exist(ticket)).thenReturn(false);
 
         LatestTicketsTravelPayoutsPopulationStage stage = new LatestTicketsTravelPayoutsPopulationStage(travelPayoutsService, subscriptionService, ticketService);
-        StageResult call = stage.call();
-        assertEquals(1, call.getSavedObjects());
+        StageResult stageResult = stage.call();
+        log.info("{}", stageResult);
+        assertEquals(1, stageResult.getSavedObjects());
     }
 
     @Test
     void if_ticket_not_exist_ticket_should_not_be_saved() {
-        TravelPayoutsService travelPayoutsService = mock(TravelPayoutsService.class);
-        SubscriptionService subscriptionService = mock(SubscriptionService.class);
-        TicketService ticketService = mock(TicketService.class);
-
         LatestRequest ledMowLatestRequest = LatestRequest.builder()
                 .origin("A")
                 .destination("B")
@@ -74,12 +81,38 @@ class LatestTicketsTravelPayoutsPopulationStageTest {
         when(ticketService.exist(ticket)).thenReturn(true);
 
         LatestTicketsTravelPayoutsPopulationStage stage = new LatestTicketsTravelPayoutsPopulationStage(travelPayoutsService, subscriptionService, ticketService);
-        StageResult call = stage.call();
-        assertEquals(0, call.getSavedObjects());
+        StageResult stageResult = stage.call();
+        log.info("{}", stageResult);
+        assertEquals(0, stageResult.getSavedObjects());
     }
 
     @Test
-    void ticketservice_exist_method_should_compare_tickets_as_expected() {
-        throw new NotImplementedException("Test is not ready yet");
+    void exist_method_should_compare_saved_tickets_as_expected() {
+        Ticket complexTicket = new Ticket();
+        complexTicket.setOrigin("A");
+        complexTicket.setDestination("B");
+        complexTicket.setValue(1234567);
+        complexTicket.setFlightNumber("C");
+        complexTicket.setDepartDate(LocalDate.now());
+        complexTicket.setDepartTime(LocalTime.now());
+        ticketService.save(complexTicket);
+
+        LatestRequest ledMowLatestRequest = LatestRequest.builder()
+                .origin("A")
+                .destination("B")
+                .limit(1000)
+                .one_way(true)
+                .build();
+
+        List<Ticket> ticketList = List.of(complexTicket);
+        when(travelPayoutsService.getLatest(ledMowLatestRequest)).thenReturn(ticketList);
+        Multimap<String, String> subscriptionDestinations = ArrayListMultimap.create(1, 1);
+        subscriptionDestinations.put("A", "B");
+        when(subscriptionService.findDistinctOriginAndDestination()).thenReturn(subscriptionDestinations);
+
+        LatestTicketsTravelPayoutsPopulationStage stage = new LatestTicketsTravelPayoutsPopulationStage(travelPayoutsService, subscriptionService, ticketService);
+        StageResult stageResult = stage.call();
+        log.info("{}", stageResult);
+        assertEquals(0, stageResult.getSavedObjects());
     }
 }
