43d2de0b322b7e94e735d785a0680a9fd71e1c8f
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-01-28 13:44:16 +0300

    52

--- src/main/java/com/example/tickets/web/OwnerController.java
+++ src/main/java/com/example/tickets/web/OwnerController.java
@@ -2,8 +2,6 @@ package com.example.tickets.web;
 
 import com.example.tickets.owner.OwnerDTO;
 import com.example.tickets.owner.OwnerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OwnerController {
-    private final Logger log = LoggerFactory.getLogger(OwnerController.class);
+    private static final String OWNER_PAGE = "owner";
     private final OwnerService service;
 
     public OwnerController(OwnerService ownerService) {
@@ -22,20 +20,20 @@ public class OwnerController {
     @GetMapping("/admin/owner")
     public String ownerPage(Model model) {
         model.addAttribute("ownerDTO", new OwnerDTO());
-        return "owner";
+        return OWNER_PAGE;
     }
 
     @PostMapping("/admin/owner/delete")
     public String ownerDelete(@ModelAttribute OwnerDTO ownerDTO) {
         Long id = ownerDTO.getId();
         service.delete(id);
-        return "owner";
+        return OWNER_PAGE;
     }
 
     @PostMapping("/admin/owner/add")
     public String ownerAdd(@ModelAttribute OwnerDTO ownerDTO) {
         service.save(ownerDTO);
-        return "owner";
+        return OWNER_PAGE;
     }
 
 }
--- src/main/java/com/example/tickets/web/SubscriptionController.java
+++ src/main/java/com/example/tickets/web/SubscriptionController.java
@@ -2,8 +2,6 @@ package com.example.tickets.web;
 
 import com.example.tickets.subscription.SubscriptionDTO;
 import com.example.tickets.subscription.SubscriptionService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +17,7 @@ import java.util.Date;
 
 @Controller
 public class SubscriptionController {
-    private final Logger log = LoggerFactory.getLogger(SubscriptionController.class);
+    private static final String SUBSCRIPTION_PAGE = "subscription";
     private final SubscriptionService service;
 
     public SubscriptionController(SubscriptionService service) {
@@ -37,20 +35,20 @@ public class SubscriptionController {
     @GetMapping("/admin/subscription")
     public String subscriptionPage(Model model) {
         model.addAttribute("subscriptionDTO", new SubscriptionDTO());
-        return "subscription";
+        return SUBSCRIPTION_PAGE;
     }
 
     @PostMapping("/admin/subscription/add")
     public String subscriptionAdd(@ModelAttribute SubscriptionDTO subscriptionDTO) {
         service.save(subscriptionDTO);
-        return "subscription";
+        return SUBSCRIPTION_PAGE;
     }
 
     @PostMapping("/admin/subscription/delete")
     public String subscriptionDelete(@ModelAttribute SubscriptionDTO subscriptionDTO) {
         Long id = subscriptionDTO.getId();
         service.delete(id);
-        return "subscription";
+        return SUBSCRIPTION_PAGE;
     }
 
 }
