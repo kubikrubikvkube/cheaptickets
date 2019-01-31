cfc0b2d6cabef48093e4ff1677dc4bc3bb497e29
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-01-31 17:59:45 +0300

    56

--- src/main/java/com/example/tickets/web/MainController.java
+++ src/main/java/com/example/tickets/web/MainController.java
@@ -1,18 +1,25 @@
 package com.example.tickets.web;
 
 import com.example.tickets.owner.OwnerDTO;
+import com.google.common.base.Strings;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     private final String MAIN_PAGE = "main";
+    private final String LOGIN_PAGE = "login";
+
 
     @RequestMapping("/main")
-    public String main(@ModelAttribute OwnerDTO ownerDTO, Model model) {
-        //сюда приходит ownerDTO из модели логина с заполненным (или нет) emailом
+    public String main(@ModelAttribute OwnerDTO ownerDTO, Model model, BindingResult bindingResult) {
+        if (ownerDTO == null) return LOGIN_PAGE;
+        var userEmail = ownerDTO.getEmail();
+        if (Strings.isNullOrEmpty(userEmail)) return LOGIN_PAGE;
+
         return MAIN_PAGE;
     }
 
