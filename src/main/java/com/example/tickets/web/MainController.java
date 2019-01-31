265a2ca698263d75e7e580a696ac85bc0ec4adfc
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-01-31 18:18:25 +0300

    56

--- src/main/java/com/example/tickets/web/MainController.java
+++ src/main/java/com/example/tickets/web/MainController.java
@@ -4,6 +4,7 @@ import com.example.tickets.owner.OwnerDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,7 +17,7 @@ public class MainController {
     private final String LOGIN_PAGE = "login";
 
 
-    public static boolean isValidEmailAddress(String email) {
+    private static boolean isValidEmailAddress(String email) {
         boolean result = true;
         try {
             InternetAddress emailAddr = new InternetAddress(email);
@@ -28,11 +29,11 @@ public class MainController {
     }
 
     @RequestMapping("/main")
-    public String main(@ModelAttribute OwnerDTO ownerDTO, Model model, BindingResult bindingResult) {
-        if (ownerDTO == null) return LOGIN_PAGE;
+    public String main(@CookieValue("JSESSIONID") String jsessionid, @ModelAttribute OwnerDTO ownerDTO, Model model, BindingResult bindingResult) {
+        if (ownerDTO == null || bindingResult.hasErrors()) return LOGIN_PAGE;
         var userEmail = ownerDTO.getEmail();
         if (!isValidEmailAddress(userEmail)) return LOGIN_PAGE;
-
+        //TODO можно запросто сделать автоматиический логин завязавшись на jsessionid!!!
         return MAIN_PAGE;
     }
 
