549cf3d7ed0d196ed4c418fec50debdd6caf0576
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-02-08 16:32:41 +0300

    1.0.13

--- src/main/java/com/example/tickets/util/RouteComparators.java
+++ src/main/java/com/example/tickets/util/RouteComparators.java
@@ -1,20 +1,22 @@
 package com.example.tickets.util;
 
 import com.example.tickets.route.Route;
-import lombok.Getter;
 
 import java.util.Comparator;
 
-public class RouteComparator {
+public class RouteComparators {
     private final static int PRICE_WEIGHT = 80;
     private final static int NUMBER_OF_CHANGES_WEIGHT = 20;
 
-    @Getter(lazy = true)
-    private final static Comparator<Route> WEIGHTED_SUM_MODEL = (r1, r2) -> {
+    public final static Comparator<Route> WEIGHTED_SUM_MODEL = (r1, r2) -> {
         var r1Weight = priceWeight(r1) - numberOfChangesWeight(r1);
         var r2Weight = priceWeight(r2) - numberOfChangesWeight(r2);
         return Integer.compare(r1Weight, r2Weight);
     };
+
+    private RouteComparators() {
+
+    }
 
     private static int priceWeight(Route route) {
         var departTicket = route.getDepartTicket();
