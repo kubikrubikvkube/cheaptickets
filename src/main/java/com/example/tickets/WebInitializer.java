77095d7a4cd217fd6bf680db8a4a39ea689467ac
Vyacheslav.Raskulin <Vyacheslav.Raskulin@nordigy.ru>
2019-02-18 13:22:56 +0300

    1.1.0

---
+++ src/main/java/com/example/tickets/WebInitializer.java
@@ -0,0 +1,13 @@
+package com.example.tickets;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TicketsApplication.class);
+    }
+
+}
